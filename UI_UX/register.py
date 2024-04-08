308640fb10596ec9c64bf4bdcd69c5de40fdb1e7
creevid <migueldavidjansenalfredo@gmail.com>
2024-04-08 10:01:21 -0400

    pantalla centrada

--- UI_UX/register.py
+++ UI_UX/register.py
@@ -20,7 +20,7 @@ def center_window(window, width, height):
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
     x = (screen_width - width) // 2
-    y = (screen_height - height) // 2
+    y = (screen_height - height) // 4
     window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
     
 # Configurar la ventana en el centro
