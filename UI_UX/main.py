0f1ed33aa20f4782e1cc675fb6c0b5748b988524
Gertrudio <jostinbautista20.10.2006@gmail.com>
2024-04-30 09:59:09 -0400

    Historial de conversación

--- UI_UX/main.py
+++ UI_UX/main.py
@@ -1,157 +1,160 @@
-
 import customtkinter
 from PIL import Image, ImageTk
 import sys
-import requests
-import json
 import threading
-import time
-import replicate
-import os
 import google.generativeai as genai
 import key
+import tkinter as tk
+from tkinter import Listbox
 
-customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+# Configuración de la apariencia y el tema
+customtkinter.set_appearance_mode("Dark")  # Modos: "System" (estándar), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Temas: "blue" (estándar), "green", "dark-blue"
 
 class App(customtkinter.CTk):
     def __init__(self, user_email):
         super().__init__()
 
-        # configure window
         self.title("ROSSE AI")
         self.geometry(f"{1100}x{580}")
         self.iconbitmap("./imagenes/a.ico")
+        self.configure_grid()
+        self.create_sidebar(user_email)
+        self.create_main_area()
+        self.create_conversation_history_frame()
+        self.create_footer()
+        self.load_api_key()
 
-        # configure grid layout (4x4)
+    def configure_grid(self):
+        # Configura el diseño de la cuadrícula (4x4)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
-        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+    def create_sidebar(self, user_email):
+        # Crea la barra lateral con widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0, fg_color="#212121")
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
 
-        # create new chat icon
-        image_path = "./imagenes/a.png"
-        image = Image.open(image_path)
-        image = image.resize((25, 20))
+        # Icono de nuevo chat
+        self.create_icon("./imagenes/a.png", " Nuevo Chat", self.sidebar_frame, 25, 20, 0)
+
+        # Icono de usuario
+        self.create_icon("./imagenes/user.png", "    " + user_email, self.sidebar_frame, 20, 20, 5)
+
+    def create_icon(self, path, text, frame, width, height, row):
+        image = Image.open(path)
+        image = image.resize((width, height))
         photo = ImageTk.PhotoImage(image)
-        new_chat_icon = customtkinter.CTkLabel(self.sidebar_frame, image=photo, text=" Nuevo Chat", compound='left')
-        new_chat_icon.grid(row=0, column=0, padx=20, pady=(10, 10))
+        label = customtkinter.CTkLabel(frame, image=photo, text=text, compound='left')
+        label.image = photo  # keep a reference!
+        label.grid(row=row, column=0, padx=20, pady=(10, 10))
 
-        # create another icon
-        image_path = "./imagenes/newchat.png"
-        image = Image.open(image_path)
-        image = image.resize((15, 15))
-        photo = ImageTk.PhotoImage(image)
-        another_icon = customtkinter.CTkLabel(self.sidebar_frame, image=photo, text="")
-        another_icon.grid(row=0, column=3, padx=20, pady=(10, 10))
-
-        
-
-        # create user icon
-
-        
-        image_path = "./imagenes/user.png"
-        image = Image.open(image_path)
-        image = image.resize((20, 20))
-        photo = ImageTk.PhotoImage(image)
-        user_email_label = customtkinter.CTkLabel(self.sidebar_frame, text="    " + user_email, compound='left')
-        user_email_label.grid(row=5, column=0, padx=20, pady=(10, 10))
-       
-
-        # create main entry and button
+    def create_main_area(self):
+        # Crea la zona principal
         self.entry = customtkinter.CTkEntry(self, placeholder_text="Habla con ROSSE")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        self.textbox = customtkinter.CTkTextbox(self, width=1000, height=650, state="disabled", fg_color="#212121", font=("Helvetica bold",12))
+        self.textbox.grid(row=0, column=1, columnspan=4, padx=(40, 40), pady=(40, 10), sticky="nsew")
 
-        # create a btn enviar
-        image_path = "./imagenes/enviar.png"
-        image = Image.open(image_path)
-        image = image.resize((20, 20))
-        photo = ImageTk.PhotoImage(image)
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="#00c4cc", corner_radius=20, height=40, width=6,  hover_color=("gray75", "gray20"), text="", image=photo)
-        self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-        self.main_button_1.configure(command=self.enviar_a_api)
+    def create_conversation_history_frame(self):
+    # Configura el frame para el historial de conversaciones
+        self.conversation_history_frame = customtkinter.CTkFrame(self.sidebar_frame, corner_radius=0)
+        self.conversation_history_frame.grid(row=1, column=0, sticky="nsew", pady=(20, 0))
+        self.conversation_history_frame.grid_rowconfigure(0, weight=1)
+        self.conversation_history_frame.grid_columnconfigure(0, weight=1)
+        
+        bg_color = self.sidebar_frame.cget("bg")
+        fg_color = ""
 
+        # Crea una lista para mostrar el historial
+        self.conversation_listbox = customtkinter.CTkListbox(self.conversation_history_frame)
+        self.conversation_listbox.grid(row=0, column=0, sticky="nsew", padx=10)
 
-        # crear botón de micrófono pequeño y redondo
-        image_path = "./imagenes/microphone.png"
-        image = Image.open(image_path)
-        image = image.resize((20, 20))
-        photo = ImageTk.PhotoImage(image)
-        self.microphone_button = customtkinter.CTkButton(self, fg_color="#00c4cc", corner_radius=20, height=40, width=4,text="",  hover_color=("gray75", "gray20"), image=photo)
-        self.microphone_button.image = image
+    def create_footer(self):
+        # Crea el footer con botones de enviar y micrófono
+        self.create_send_button()
+        self.create_microphone_button()
+
+    def create_send_button(self):
+        self.send_button = self.create_button("./imagenes/enviar.png", "", self.send_to_api, 20, 20)
+        self.send_button.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+    def create_microphone_button(self):
+        self.microphone_button = self.create_button("./imagenes/microphone.png", "", None, 20, 20)
         self.microphone_button.grid(row=3, column=4, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        # create textbox
-        self.textbox = customtkinter.CTkTextbox(self, width=1000, height=650)
-        self.textbox.grid(row=0, column=1, columnspan=4, padx=(40, 40), pady=(40, 10), sticky="nsew")
-       
-        # establecer textbox en solo lectura
-        self.textbox.configure(state="disabled")
-
-        # create image and text to insert into textbox
-        image_path = "./imagenes/a.png"
-        image = Image.open(image_path)
-        image = image.resize((350, 300))
+    def create_button(self, path, text, command, width, height):
+        image = Image.open(path)
+        image = image.resize((width, height))
         photo = ImageTk.PhotoImage(image)
-        text = "¿Cómo puedo ayudarte hoy?"
+        button = customtkinter.CTkButton(self, image=photo, text=text, command=command, fg_color="#00c4cc", corner_radius=20, height=40, width=6, hover_color=("gray75", "gray20"))
+        button.image = photo  # keep a reference!
+        return button
 
-        # create a label to display the image and text
-        self.label = customtkinter.CTkLabel(self, text=text, image=photo, compound="top")
-        self.label.grid(row=0, column=1, columnspan=4, padx=(40, 40), pady=(40, 10), sticky="nsew")
-
-        # configure the font and weightof the text
-        self.label.configure(font=("Arial black", 24))
-
-        # schedule the deletion of the label after 3 seconds
-        self.after(10000, self.delete_label)
-
-    def delete_label(self):
-        # delete the label
-        self.label.destroy()
-
-
-    def enviar_a_api(self):
-     # Tomar el texto de entrada
+    def send_to_api(self):
+        # Toma el texto de entrada y lo envía a la API
         input_text = self.entry.get()
         self.textbox.configure(state='normal')
         self.textbox.insert('end', f"\nTú: {input_text}\n\n")
-        self.entry.delete(0, 'end')  # Limpiar la entrada después de enviar
-
-        # Deshabilitar la entrada y el botón mientras se obtiene la respuesta
+        self.update_conversation_history(f"Tú: {input_text}")
+        self.entry.delete(0, 'end')  # Limpia la entrada después de enviar
         self.entry.configure(state='disabled')
-        self.main_button_1.configure(state='disabled')
+        self.send_button.configure(state='disabled')
+        threading.Thread(target=self.get_response_from_api, args=(input_text,)).start()
 
-        # Iniciar un nuevo hilo para la solicitud de la API
-        threading.Thread(target=self.obtener_respuesta_api, args=(input_text,)).start()
-        threading.Thread(target=self.obtener_respuesta_api, args=(input_text,)).start()
- 
-    def obtener_respuesta_api(self, input_text):
+    def get_response_from_api(self, input_text):
+     try:
         genai.configure(api_key=key.clave)
         model = genai.GenerativeModel(model_name="gemini-pro")
+        
+        print(f"Enviando a la API: {input_text}")  # Debug print, remove in production
+        response = model.generate_content(input_text)
 
-        # Realizar la consulta y obtener la respuesta
-        consulta = input_text
-        response = model.generate_content(consulta)
-
-        # Actualizar el cuadro de texto con la respuesta de la API
-        self.textbox.insert('end', f"\nROSSE AI: {response.text}\n")
-
-        # Restablecer el estado de la entrada y el botón
-        self.textbox.configure(state='disabled')
+        if response.text:
+            print(f"Respuesta de la API: {response.text}")  # Debug print, remove in production
+            self.textbox.configure(state='normal')
+            self.textbox.insert('end', f"\nROSSE AI: {response.text}\n")
+            self.textbox.configure(state='disabled')
+        else:
+            print("No hubo respuesta de la API.")  # Debug print, remove in production
+     except Exception as e:
+        print(f"Se produjo un error al obtener la respuesta de la API: {e}")  # Debug print, remove in production
+     finally:
+        # Restablecer el estado de la entrada y el botón independientemente del resultado
         self.entry.configure(state='normal')
-        self.main_button_1.configure(state='normal')
+        self.send_button.configure(state='normal')
         self.textbox.see('end') 
-    # Cargar la API key desde una variable de entorno
+
+# Recuerda remover los print statements antes de lanzar la aplicación.
+
+
+    def update_conversation_history(self, message):
+        # Inserta el nuevo mensaje al principio de la lista
+        self.conversation_listbox.insert(0, message)
+        # Limita la cantidad de mensajes mostrados
+        if self.conversation_listbox.size() > 100:
+            self.conversation_listbox.delete(100)
+
+    def load_api_key(self):
+        # Carga la API key desde una variable de entorno
+        # Esta función debe implementarse con tu lógica de carga de clave API
+        pass
     
+    
+    def create_conversation_history_frame(self):
+        # Configura el frame para el historial de conversaciones
+        self.conversation_history_frame = customtkinter.CTkFrame(self.sidebar_frame, corner_radius=0)
+        self.conversation_history_frame.grid(row=1, column=0, sticky="nsew", pady=(20, 0))
+        self.conversation_history_frame.grid_rowconfigure(0, weight=1)
+        self.conversation_history_frame.grid_columnconfigure(0, weight=1)
+
+        # Crea un Listbox estándar de Tkinter para mostrar el historial
+        self.conversation_listbox = Listbox(self.conversation_history_frame, bg="#212121", fg="white", borderwidth=0, highlightthickness=0)
+        self.conversation_listbox.grid(row=0, column=0, sticky="nsew", padx=10)
 
 if __name__ == "__main__":
-    # Recupera el correo electrónico del argumento si existe, de lo contrario usa un valor predeterminado
     email_arg = sys.argv[1] if len(sys.argv) > 1 else "username@fulanito"
     app = App(email_arg)
     app.mainloop()
-
-
